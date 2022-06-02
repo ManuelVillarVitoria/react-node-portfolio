696faf2328a73058be006513ef8dd45aade6c26f
Manu <manu.villar.vitoria@gmail.com>
2022-06-03 01:13:34 +0200

    styling profile component

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typical from "react-typical";
-import codesandbox from "../../images/codesandbox.png";
+import codesandbox from "../../assets/images/codesandbox.png";
+import "./Profile.css"
 
 const Profile = () => {
   return (
@@ -8,25 +9,29 @@ const Profile = () => {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.linkedin.com/in/mwfront/" target="_blank">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
-            <a href="https://github.com/ManuelVillarVitoria" target="_blank">
-              <i className="fa fa-github-square"></i>
-            </a>
-            <a href="https://medium.com/@manuelvillar_19482" target="_blank">
-              <i className="fa fa-brands fa-medium"></i>
-            </a>
-            <a
-              href="https://codesandbox.io/dashboard/all/?workspace=d705a9d9-5ec2-4b59-86a2-4aae8af77374"
-              target="_blank"
-            >
-              <img
-                src={codesandbox}
-                className="codesandbox"
-                alt="codesandbox"
-              />
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/mwfront/" target="_blank" rel="noreferrer" title="Linkedin">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/ManuelVillarVitoria" target="_blank" rel="noreferrer" title="Github">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://medium.com/@manuelvillar_19482" target="_blank" rel="noreferrer" title="Medium">
+                <i className="fa fa-brands fa-medium"></i>
+              </a>
+              <a
+                href="https://codesandbox.io/dashboard/all/?workspace=d705a9d9-5ec2-4b59-86a2-4aae8af77374"
+                target="_blank"
+                rel="noreferrer" 
+              >
+                <img
+                  src={codesandbox}
+                  className="codesandbox"
+                  alt="codesandbox"
+                  title="CodeSandbox"
+                />
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -37,7 +42,7 @@ const Profile = () => {
           </div>
           <div className="profile-details-role">
             <span className="primary-text">
-              {" "}
+              {""}
               <h1>
                 {""}
                 <Typical
@@ -71,6 +76,9 @@ const Profile = () => {
               <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
