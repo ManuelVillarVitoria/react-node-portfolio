5a9c3db1d8f97e7b8985b1c168caa0e61665928e
Manu <manu.villar.vitoria@gmail.com>
2022-06-09 16:51:09 +0200

    creating ScreenHeading reusable component

---
+++ client/src/utilities/ScreenHeading/ScreenHeading.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ScreenHeading({ title, subHeading }) {
+  return (
+    <div className="heading-container">
+      <div className="screen-heading">
+        <span>{title}</span>
+      </div>
+
+      {subHeading ? (
+        <div className="screen-sub-heading">
+          <span>{subHeading}</span>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      <div className="heading-seperator">
+        <div className="separator-line">
+          <div className="separator-blob">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
