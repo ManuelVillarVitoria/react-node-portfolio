0572689049bb467c2e28ec1c414f715c9570485c
Manu <manu.villar.vitoria@gmail.com>
2022-06-09 12:11:16 +0200

    Navigations and Click Functionality

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {
-  TOTAl_SCREENS,
+  TOTAL_SCREENS,
   GET_SCREEN_INDEX,
 } from "../../../utilities/commonUtils";
 import ScrollService from "../../../utilities/ScrollService";
@@ -20,7 +20,64 @@ const Header = () => {
   let currentScreenSubscription =
     ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
-  return <div></div>;
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option";
+    if (index < TOTAL_SCREENS.lenght - 1) classes += "header-option-seperator";
+
+    if (selectedScreen === index) classes += "selected-header-option";
+    return;
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
+
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+
+  return (
+    <div>
+      <div
+        className="header-option"
+        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+      >
+        <div className="header-parent">
+          <div
+            className="header-hamburger"
+            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+          >
+            <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+          </div>
+          <div className="header-logo">
+            <span>ManuDEV~</span>
+          </div>
+          <div
+            className={
+              showHeaderOptions
+                ? "header-options show-hamburger-options"
+                : "header-options"
+            }
+          >
+            {getHeaderOptions()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
